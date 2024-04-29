59e4cc6fb56b122ddf1813e079a9835645528a07
Lucas Barroso <lucas.jorge@sptech.school>
2024-04-29 19:50:56 -0300

    feat: telas refatoradas

--- mobile/app/src/main/java/com/example/codegenius/feature/aluno/login/views/states/LoginScreenUiState.kt
+++ mobile/app/src/main/java/com/example/codegenius/feature/aluno/login/views/states/LoginScreenUiState.kt
@@ -1,12 +1,25 @@
 package com.example.codegenius.feature.aluno.login.views.states
 
-import android.content.Intent
-import androidx.core.content.ContextCompat.startActivity
-import com.example.codegenius.feature.aluno.course.view.CourseActivity
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
+import androidx.compose.ui.graphics.vector.ImageVector
 
 data class LoginScreenUiState (
     val email: String = "",
     val password: String = "",
     val onEmailChange: (String) -> Unit = {},
-    val onPasswordChange: (String) -> Unit = {}
-)
+    val onPasswordChange: (String) -> Unit = {},
+    var visibility: Boolean = false,
+) {
+    val visibilityChange: () -> Unit = {
+        visibility = !visibility
+    }
+    val eyeState: () -> ImageVector =  {
+        if(visibility){
+            Icons.Outlined.Visibility
+        } else{
+            Icons.Outlined.VisibilityOff
+        }
+    }
+}
